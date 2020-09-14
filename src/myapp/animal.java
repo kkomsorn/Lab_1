2e028f85f9617d41b4e7bd8fbd56523d58afc183
komsorn thongboonma <komsorn.th@gmail.com>
2020-09-14 17:38:15 +0700

    add constructor

--- src/myapp/animal.java
+++ src/myapp/animal.java
@@ -2,4 +2,8 @@ package myapp;
 
 public class animal {
     private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/myapp/main.java
+++ src/myapp/main.java
@@ -2,6 +2,6 @@ package myapp;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello The World");
+        System.out.println("Hello To The World");
     }
 }
